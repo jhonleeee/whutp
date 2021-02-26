06cee88c98738f8a79cd04b46ccc314be15d9a4d
jhonleeee <47035602+jhonleeee@users.noreply.github.com>
2021-02-26 08:19:09 +0000

    Deploying to gh-pages from  @ 2198be0ccad8678da0da46e2d13495ab97fef081 🚀

---
+++ precache-manifest.b64f33dd1ab20419f53c3675e025b175.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e5d2c17bc62f6a6a46",
+    "url": "/whutp/css/app.92e6ac50.css"
+  },
+  {
+    "revision": "263221b13dd9dabac7e4",
+    "url": "/whutp/css/chunk-vendors.7452ea5d.css"
+  },
+  {
+    "revision": "8822400de4a6dfa87c4decce43fedeac",
+    "url": "/whutp/img/drawerImg.8822400d.jpeg"
+  },
+  {
+    "revision": "2e382c92034dcda37ecaf8020ad70b11",
+    "url": "/whutp/img/memphis-colorful-dark.2e382c92.png"
+  },
+  {
+    "revision": "8b33b22920514e7ead6b8635a751ac07",
+    "url": "/whutp/img/memphis-colorful.8b33b229.png"
+  },
+  {
+    "revision": "7813968a0e4f5be6abdfa3dea82294c0",
+    "url": "/whutp/index.html"
+  },
+  {
+    "revision": "83e5d2c17bc62f6a6a46",
+    "url": "/whutp/js/app.05948bc9.js"
+  },
+  {
+    "revision": "263221b13dd9dabac7e4",
+    "url": "/whutp/js/chunk-vendors.23f7740e.js"
+  },
+  {
+    "revision": "96bf558827d4e4edef88025fb780a789",
+    "url": "/whutp/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/whutp/robots.txt"
+  }
+]);
